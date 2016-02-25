6a3ab32705bda59e4cebb3db30a7c91402fbc607
lily healey <lily@lilyhealey.co.uk>
2016-02-25 13:08:21 -0500

    make urls directory-independent

--- index.php
+++ index.php
@@ -1,22 +1,29 @@
 <?php
+require_once("views/head.php");
 
-// $uri is of the form /admin/$view/$o[url]/$o[url]/...
-// views are in folder ./views
-$uri = explode('/', $_SERVER['REQUEST_URI']);
-$view = "views/";
-$view.= $uri[2] ? $uri[2]: "cover";
-$view.= ".php";
+// UGH THIS IS HIDEOUS
+// $view is declared in head.php, in url_array()
+// (should definitely be somewhere else)
+// else is for backwards compatibility (maybe not necessary?)
+if($view)
+	$view_path = "views/".$view.".php";
+else
+{
+	$uri = explode('/', $_SERVER['REQUEST_URI']);
+	$view_path = "views/";
+	$view_path.= $uri[2] ? $uri[2]: "cover";
+	$view_path.= ".php";
+}
 
 try {
-	if(!file_exists($view))
+	if(!file_exists($view_path))
 		throw new Exception("404");
 }
 catch(Exception $e) {
-	$view = "views/errors/".$e->getMessage().".php";
+	$view_path = "views/errors/".$e->getMessage().".php";
 }
 
-require_once("views/head.php");
-require_once($view);
+require_once($view_path);
 require_once("views/foot.php"); 
 
 ?>

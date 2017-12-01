ac222323c6e138cd09ce25190d894f4350b88516
Eric Li <ericyoungli@gmail.com>
2017-11-30 23:49:36 -0500

    Update lib.php

--- lib/lib.php
+++ lib/lib.php
@@ -144,18 +144,18 @@ function process_media($toid)
 	{
 		if($error == UPLOAD_ERR_OK)
 		{
+			$tmp_name = $_FILES["uploads"]["tmp_name"][$key];
+			$m_name = $_FILES["uploads"]["name"][$key];
+			$m_type = strtolower(end(explode(".", $m_name)));
+			
 			// add to db's image list
 			$m_arr["type"] = "'".$m_type."'";
 			$m_arr["object"] = "'".$toid."'";
 			$m_arr["caption"] = "'".$rr->captions[$key+count($rr->medias)]."'";
 			$insert_id = $mm->insert($m_arr);
-
-			$tmp_name = $_FILES["uploads"]["tmp_name"][$key];
-			$m_name = $_FILES["uploads"]["name"][$key];
-			$m_type = strtolower(end(explode(".", $m_name)));
 			$m_rows++;
-			$m_file = m_pad($insert_id).".".$m_type;
 
+			$m_file = m_pad($insert_id).".".$m_type;
 			$m_dest = $resize ? $resize_root : $media_root;
 			$m_dest.= $m_file;
 

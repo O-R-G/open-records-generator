3e15b2daac42fa5ae02511111542f7dc726c8136
david reinfurt <reinfurt@o-r-g.com>
2017-12-08 16:02:46 -0500

    fix url.php to work without passed $urls array -- no longer requires second url.php at root in ../config/url.php

--- config/url.php
+++ config/url.php
@@ -1,7 +1,10 @@
 <?php
+/*
+    class for dealing with URLs
+*/
 
-class URL extends URL_Base
-{	
+class URL extends URL_Base {
+	
 	// $this->urls = array of all object urls
 	// $this->url = this object's url
 	// $this->ids = array of all object ids
@@ -9,23 +12,35 @@ class URL extends URL_Base
 	// if no object selected, 
 	// $this->urls, $this->url, and $this->ids are not set
 	// $this->id is 0
-	function __construct($urls=null)
-	{
+    
+	function __construct($urls=null) {
+
 		global $oo;
-		
-		try 
-		{
+
+        if (!$urls) {
+            // get script url and request url
+            $script = explode('/', rtrim($_SERVER['SCRIPT_NAME'], '/'));    
+            $urls = explode('/', rtrim($_SERVER['REQUEST_URI'], '/'));
+
+            // compare and shift until they diverge
+            while($script[0] == $urls[0]) {
+                array_shift($script);
+                array_shift($urls);
+            }
+        }
+        print_r($urls);
+
+		// check that the object that this URL refers to exists
+		try {
 			$ids = $oo->urls_to_ids($urls);
 		}
-		// check that the object that this URL refers to exists
-		// FIX THIS CODE
-		catch(Exception $e)
-		{
+		catch(Exception $e) {
 			$urls = array_slice($urls, 0, $e->getMessage());
 			$ids = $oo->urls_to_ids($urls);
-			$loc = $host.implode("/".$base)."/".implode("/", $urls);
-			header("Location: ".$loc);
+			// $loc = $host.implode("/".$base)."/".implode("/", $urls);
+			// header("Location: ".$loc);
 		}
+
 		$id = $ids[count($ids)-1];
 		if(!$id)
 			$id = 0;
@@ -39,6 +54,29 @@ class URL extends URL_Base
 		$this->url_str = implode($urls);
 		// $this->back_url = 
 	}
-}
 
+    public function parents()
+    {
+        global $oo;
+        global $admin_path;
+        $urls = $this->urls;
+        $ids = $this->ids;
+        $parents[] = "";
+
+        for($i = 0; $i < count($urls)-1; $i++)
+        {
+            $parents[$i]['url'] = $admin_path."browse/";
+            for($j = 0; $j < $i + 1; $j++)
+            {
+                $parents[$i]['url'].= $urls[$j];
+                if($j < $i)
+                    $parents[$i]['url'].= "/";
+            }
+            $parents[$i]["name"] = $oo->name($ids[$i]);
+        }
+        if($parents[0] == "")
+            unset($parents);
+        return $parents;
+    }
+}
 ?>

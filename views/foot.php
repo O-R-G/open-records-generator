5c256e1eb37d38af86a20f7e8dbf7c419f24a00c
Eric Li <ericyoungli@gmail.com>
2019-04-01 17:49:59 -0400

    remove sync pb

--- views/foot.php
+++ views/foot.php
@@ -9,8 +9,7 @@ $g = $host.$generate_url;
 						<?php if ($user != 'guest'): ?>
 							<a class="button" href="<? echo $admin_path; ?>settings">SETTINGS</a>
 						<?php endif; ?>
-						<a class="button" href="<? echo $admin_path; ?>sync-spektrix">SYNC SPEKTRIX</a>
-						<a class="button" href="<? echo $admin_path; ?>sync-pb">SYNC PB (DEPRECATED)</a>
+						<a class="button" href="<? echo $admin_path; ?>sync-spektrix">SYNC</a>
 						<a class="button" href="<? echo $admin_path; ?>logout" style="float: right;">LOG OUT</a>
 					<? endif; ?>
 				</footer>

97d03f8195311b8bf52170918c7133469137bd36
david reinfurt <reinfurt@o-r-g.com>
2020-06-02 11:33:52 -0400

    add static/license with hash

--- views/info.php
+++ views/info.php
@@ -3,17 +3,20 @@
 // fopen wrappers?
 // see 'notes' on this page:
 // http://php.net/manual/en/function.file-get-contents.php
-$license_file = $admin_root."static/gnu.txt";
+$license_file = $admin_root."static/license.txt";
 $license = file_get_contents($license_file);
+$license_gnu_file = $admin_root."static/gnu.txt";
+$license_gnu = file_get_contents($license_gnu_file);
 ?><div id="body-container">
 	<div id="body">
 		<div class="self-container">
 			<p>
 				Open Records Generator<br>
+                <? echo nl2br($license); ?>
 				<a href="http://www.o-r-g.com/" target="_blank">O R G inc.</a>
 			</p>
 			<div class="license-container">
-				<textarea class="large"><? echo $license; ?></textarea>
+				<textarea class="large"><? echo $license_gnu; ?></textarea>
 			</div>
 			<div>
 				<a href="<? echo $js_back; ?>">&lt; RETURN</a>

f348324446f43f34c46233cc1ff11d0fbc7d3550
wei <wei@o-r-g.com>
2022-04-21 11:43:07 +0800

    update url.php to recognize query strings

--- config/url.php
+++ config/url.php
@@ -18,16 +18,15 @@ class URL extends URL_Base {
 		global $oo;
 
 		if (!$urls) {
-            		// get script url and request url
-           	 	$script = explode('/', rtrim($_SERVER['SCRIPT_NAME'], '/'));
-        		$urls = explode('/', rtrim($_SERVER['REQUEST_URI'], '/'));
-
-        		// compare and shift until they diverge
-        		while(isset($script[0]) && isset($urls[0]) && $script[0] == $urls[0]) {
-	                array_shift($script);
-            		array_shift($urls);
-        		}
-        	}
+    		// get script url and request url
+       	 	$script = explode('/', rtrim($_SERVER['SCRIPT_NAME'], '/'));
+    		$urls = explode('/', rtrim( strtok($_SERVER['REQUEST_URI'],"?"), '/'));
+    		// compare and shift until they diverge
+    		while(isset($script[0]) && isset($urls[0]) && $script[0] == $urls[0]) {
+                array_shift($script);
+        		array_shift($urls);
+    		}
+    	}
 
 		// check that the object that this URL refers to exists
 		try {

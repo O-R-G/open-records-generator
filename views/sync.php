3e26d6e5afcf9ef1a84304e676680fe1a9e35979
wei <wei@o-r-g.com>
2023-06-06 13:52:51 +0800

    sync ux

--- views/sync.php
+++ views/sync.php
@@ -16,23 +16,26 @@
     let sync_form = document.getElementById('sync-form');
     let request_url = '<?php echo $request_url; ?>';
     let request = new XMLHttpRequest();
+    let div_syncing = document.createElement('DIV');
+    div_syncing.innerText = 'Syncing . . .';
     request.onreadystatechange = function(){
         if(request.readyState == 4 && request.status == 200)
         {
             let response = JSON.parse(request.responseText);
             if(response['status'] == 'success')
             {
-                let div = document.createElement('DIV');
-                div.id = 'sync-result-container';
-                div.innerHTML = response['body'];
-                sync_form.parentElement.replaceChild(div, sync_form);
+                let div_report = document.createElement('DIV');
+                div_report.id = 'sync-result-container';
+                div_report.innerHTML = response['body'];
+                sync_form.parentElement.replaceChild(div_report, sync_form);
+                div_syncing.remove();
             }
         }
     }
     request.open('POST', request_url, true);
     sync_form.addEventListener('submit', function(event){
-        console.log('submit');
         event.preventDefault();
+        sync_form.parentElement.appendChild(div_syncing);
         let data = new FormData(event.target);
         request.send(data);
     });
